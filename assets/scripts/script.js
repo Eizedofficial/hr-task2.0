47269e6ec28083c36e207cf2aac0c369aedc6605
Eized <brookenlp1@gmail.com>
2022-03-01 22:23:25 +0500

    Client script updated

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,26 +1,37 @@
-$(document).ready(function(){
-    $("#submitRoutersData").click(function(){
-        event.preventDefault()
-        $.ajax({
-            url: '/addRouter.php',
-            method: 'get',
-            data: $('#routersData').serialize()
-        }).done(function(response) {
-            let errorMessage = "";
-            if(response.stored.length > 0) {
-                errorMessage += "Следующие серийные номера уже были в базе:\n";
-                response.stored.forEach((code) => errorMessage += code + "\n");
-            }
-            if(response.wrong.length > 0) {
-                errorMessage += "Следующие серийные номера не подходили по условиям валидации:\n";
-                response.wrong.forEach((code) => errorMessage += code + "\n");
-            }
+$(document).ready(function () {
+    $("#submitRoutersData").click(function () {
+        try {
+            let data = $('#routersData').serialize();
+            data = data + '&action=storeEquipment';
+            $.ajax({
+                url: '/index.php',
+                method: 'get',
+                data: data
+            }).done(function (response) {
+                if (response.status === false) {
+                    alert("Произошла ошибка.\n" + response.message);
+                } else {
+                    let errorMessage = "";
+                    if (response.data.stored.length > 0) {
+                        errorMessage += "Следующие серийные номера уже были в базе:\n";
+                        response.data.stored.forEach((code) => errorMessage += code + "\n");
+                    }
+                    if (response.data.wrong.length > 0) {
+                        errorMessage += "Следующие серийные номера не подходили по условиям валидации:\n";
+                        response.data.wrong.forEach((code) => errorMessage += code + "\n");
+                    }
 
-            if(errorMessage !== "") {
-                alert(errorMessage);
-            } else {
-                alert("Успешно");
-            }
-        });
+                    if (errorMessage !== "") {
+                        alert(errorMessage);
+                    } else {
+                        alert("Успешно");
+                    }
+                }
+            });
+        } catch (e) {
+            window.open("https://stackoverflow.com/search?q=" + e.message, "_blank");
+        }
+
+        return false;
     })
 })
